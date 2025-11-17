e43d1ddf1b51cf142f39ad1cfabb312cc760f020
mambasa <101706131+kal990574@users.noreply.github.com>
2025-11-17 12:34:19 +0900

    feat: NoteType과 Note 기본 구조 구현

---
+++ Assets/02.Scripts/Note/Note.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Note
+{
+    public NoteType Type;
+    public float TargetTime;        // judge까지 시간
+    public float SpawnTime;         // 노트 생성 시간
+
+    [System.NonSerialized]
+    public bool IsProcessed = false; // hit 처리
+
+    public Note(NoteType type, float targetTime)
+    {
+        Type = type;
+        TargetTime = targetTime;
+    }
+
+    public void CalculateSpawnTime(float travelTime)
+    {
+        SpawnTime = TargetTime - travelTime;
+    }
+}
