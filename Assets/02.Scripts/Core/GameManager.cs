8bf6e28b20ebb4e3e4738f9351561ff502fb1d64
mambasa <101706131+kal990574@users.noreply.github.com>
2025-11-17 12:28:40 +0900

    feat: GameManager 기본 구현

---
+++ Assets/02.Scripts/Core/GameManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    [Header("Game State")]
+    public GameState CurrentState = GameState.Ready;
+    
+    // 싱글톤 패턴
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        CurrentState = GameState.Ready;
+        Debug.Log("GameManager 초기화 완료");
+    }
+
+    public void StartGame()
+    {
+        CurrentState = GameState.Playing;
+        Debug.Log("게임 시작");
+    }
+
+    public void GameOver()
+    {
+        CurrentState = GameState.GameOver;
+        Debug.Log("게임 오버");
+    }
+}
+
+public enum GameState
+{
+    Ready,
+    Playing,
+    Paused,
+    GameOver
+}
