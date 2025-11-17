158465c71095253ac0b896c5fc9856895a4b256a
mambasa <101706131+kal990574@users.noreply.github.com>
2025-11-17 12:40:14 +0900

    feat: hit 판정 결과 enum 구현

---
+++ Assets/02.Scripts/Judgement/JudgementType.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public enum JudgementType
+{
+    Perfect,
+    Good,
+    Miss
+}
--- Assets/02.Scripts/Note/Note.cs
+++ Assets/02.Scripts/Note/Note.cs
@@ -6,8 +6,6 @@ public class Note
     public NoteType Type;
     public float TargetTime;        // judge까지 시간
     public float SpawnTime;         // 노트 생성 시간
-
-    [System.NonSerialized]
     public bool IsProcessed = false; // hit 처리
 
     public Note(NoteType type, float targetTime)
